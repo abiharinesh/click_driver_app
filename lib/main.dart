fe65ae9ecb10ccb685ecc480c244447f9562f8e6
master <sreclib0017@gmsail.com>
2024-10-25 12:22:09 +0530

    lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:click_driver_app/splashScreen/splash_screen.dart';
 import 'package:flutter/material.dart';
-
+// final pricess
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MySplashScreen(),
+      home: const MySplashScreen(),
     );
   }
 }
